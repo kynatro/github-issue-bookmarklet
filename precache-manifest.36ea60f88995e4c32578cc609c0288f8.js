bc3e5107173af9a02c2c44643617f2c9dc143027
dave.shepard <dave.shepard@servicenow.com>
2020-02-21 14:21:19 -0800

    Updates

---
+++ precache-manifest.36ea60f88995e4c32578cc609c0288f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50a393bd899181c5b8b962806fec40d8",
+    "url": "/github-issue-bookmarklet/index.html"
+  },
+  {
+    "revision": "a54791bb9038ea064b31",
+    "url": "/github-issue-bookmarklet/static/css/main.50c825d9.chunk.css"
+  },
+  {
+    "revision": "88bdc6f6e3d873ba8029",
+    "url": "/github-issue-bookmarklet/static/js/2.e8c042b3.chunk.js"
+  },
+  {
+    "revision": "f32f3f2739f945f6e8e72f406ee3a70f",
+    "url": "/github-issue-bookmarklet/static/js/2.e8c042b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a54791bb9038ea064b31",
+    "url": "/github-issue-bookmarklet/static/js/main.87fc6b50.chunk.js"
+  },
+  {
+    "revision": "c3cac16ef18afadd42bf",
+    "url": "/github-issue-bookmarklet/static/js/runtime-main.ee6030ae.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.41022e7579a624c173d24781e9a9bace.js"
+  "/github-issue-bookmarklet/precache-manifest.36ea60f88995e4c32578cc609c0288f8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github-issue-bookmarklet/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
